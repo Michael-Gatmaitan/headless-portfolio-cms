5fc7a038314a25e054307e22ba75810b63ba2b06
Michael Gatmaitan <michaelgatmaitan.pdm@gmail.com>
2026-05-27 11:46:54 +0800

    Refactor: api/utils/sortOrder.ts updated to use dyamic import as the error in vercel logs suggest and maked the function calls async.

--- apps/api/src/utils/sortOrder.ts
+++ apps/api/src/utils/sortOrder.ts
@@ -1,18 +1,3 @@
-// import { generateKeyBetween } from "fractional-indexing";
-
-// export function nextSortOrder(
-//   lastSortOrder: string | null | undefined,
-// ): string {
-//   return generateKeyBetween(lastSortOrder ?? null, null);
-// }
-
-// export function sortOrderBetween(
-//   before: string | null | undefined,
-//   after: string | null | undefined,
-// ): string {
-//   return generateKeyBetween(before ?? null, after ?? null);
-// }
-
 export async function nextSortOrder(lastSortOrder: string | null | undefined) {
   const { generateKeyBetween } = await import("fractional-indexing");
   return generateKeyBetween(lastSortOrder ?? null, null);
